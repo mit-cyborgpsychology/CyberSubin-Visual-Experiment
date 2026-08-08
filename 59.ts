510b85bb26c1c2880f0e320db11d9884a316f18f
pat pataranutaporn <patpat@mit.edu>
2026-08-09 01:44:34 +0700

    Init

---
+++ 59.ts
@@ -0,0 +1,190 @@
+export const posture: { thai: string; english: string; pronounce: string }[] = [
+	{ thai: 'เทพประนม', english: 'The Salutation of the Angels', pronounce: 'Thep-pra-nom' },
+	{ thai: 'ปฐม', english: 'The Beginning', pronounce: 'Pra-thom' },
+	{ thai: 'พรหมสี่หน้า', english: 'The Four-Faced Brahma', pronounce: 'Prom-see-na' },
+	{ thai: 'สอดสร้อยมาลา', english: 'Stringing a Garland', pronounce: 'Sod-soi-ma-la' },
+	{ thai: 'ช้านางนอน', english: 'Lulling a Lady to Sleep', pronounce: 'Cha-nang-non' },
+	{
+		thai: 'ผาลาเพียงไหล่',
+		english: 'A Hillock Reaching Only Up to the Shoulder ',
+		pronounce: 'Pa-la-peang-lai'
+	},
+	{ thai: 'พิศมัยเรียงหมอน', english: 'An Intimate Love', pronounce: 'Pis-sa-mai-reang-mon' },
+	{ thai: 'กังหันร่อน', english: 'A Flying Turbine', pronounce: 'Kang-han-ron' },
+	{ thai: 'ภมรเคล้า', english: 'A Bee Wallowing in a Flower', pronounce: 'Pa-morn-klaow' },
+	{
+		thai: 'แขกเต้าเข้ารัง',
+		english: 'A Parakeet Entering the Nest',
+		pronounce: 'Kak-tao-khao-rang'
+	},
+	{
+		thai: 'กระต่ายชมจันทร์',
+		english: 'A Rabbit Admiring the Moon',
+		pronounce: 'Kra-tai-chom-jan'
+	},
+	{ thai: 'จันทร์ทรงกลด', english: 'A Moon Halo', pronounce: 'Jan-song-klod' },
+	{
+		thai: 'พระรถโยนสาร',
+		english: 'Phra Rod Throwing an Epistle',
+		pronounce: 'Pra-rod-yon-sarn'
+	},
+	{ thai: 'จ่อเพลิงกาล', english: 'Lighting Up a Great Fire', pronounce: 'Jaw-plaeng-karn' },
+	{
+		thai: 'หณุมานผลาญยักษ์',
+		english: 'Hanuman Kills the Giants',
+		pronounce: 'Hanuman-plarn-yak'
+	},
+	{
+		thai: 'พระรามโก่งศร (พระหริรักษ์โก่งศิลป์)',
+		english: 'Rama Arches His Arrow',
+		pronounce: 'Pra-ram-kong-sorn'
+	},
+	{
+		thai: 'ช้างประสานงา (คชรินทร์ประสานงา)',
+		english: 'Elephants Clashing Their Tusks',
+		pronounce: 'Chang-pra-sarn-nga'
+	},
+	{
+		thai: 'ช้างสบัดหญ้า',
+		english: 'An Elephant Shaking Grasses',
+		pronounce: 'Chang-sa-bad-nha'
+	},
+	{ thai: 'ขี่ม้าตีคลี', english: 'Playing Horseback Polo', pronounce: 'Kee-ma-tee-klee' },
+	{
+		thai: 'สารถีชักรถ',
+		english: 'A Charioteer Controlling a Chariot',
+		pronounce: 'Sa-ra-tee-chak-rod'
+	},
+	{
+		thai: 'กลดพระสุเมรุ',
+		english: ' The Aureole of the Sumeru Mountain',
+		pronounce: 'Glod-pra-su-main'
+	},
+	{ thai: 'ตระเวนเวหา', english: 'Flying through the Sky', pronounce: 'Tra-wain-way-ha' },
+	{
+		thai: 'นาคาม้วนหาง',
+		english: 'Naga Rolling Its Tail',
+		pronounce: 'Na-ga-muan-hang'
+	},
+	{
+		thai: 'กวางเดินดง',
+		english: 'A Deer Wandering in a Forest',
+		pronounce: 'Kwang-dern-dong'
+	},
+	{ thai: 'หงส์ลีลา', english: 'A Swan Gliding', pronounce: 'Hong-lee-la' },
+	{ thai: 'ชักแป้งผัดหน้า', english: 'Powdering the Face', pronounce: 'Chak-pang-pad-na' },
+	{
+		thai: 'เหราเล่นน้ำ',
+		english: 'A Hera Playing in Water',
+		pronounce: 'He-ra-len-nam'
+	},
+	{
+		thai: 'กินนรเลียบถ้ำ',
+		english: 'A Kinnorn Walking Around the Cave',
+		pronounce: 'Kin-norn-leab-tam'
+	},
+	{
+		thai: 'ยังคิดประดิษฐ์รำ',
+		english: 'Creating New Dance Poses',
+		pronounce: 'Young-kid-pra-dit-tam'
+	},
+	{ thai: 'กระหวัดเกล้า', english: 'Tidying Up the Hair', pronounce: 'Kra-wat-klaow' },
+	{ thai: 'ตีโทนโยนทับ', english: 'Beating the Drum', pronounce: 'Tee-ton-yon-tap' },
+	{ thai: 'งูขว้างค้อน', english: 'Malayan Pit Viper', pronounce: 'Ngu-kwang-kon' },
+	{
+		thai: 'มัจฉาชมสาคร',
+		english: 'A Fish Swimming in a River',
+		pronounce: 'Mad-cha-chom-sa-korn'
+	},
+	{ thai: 'กินนรฟ้อนโอ้', english: 'A Kinnorn Dancing', pronounce: 'Kin-norn-forn-oh' },
+	{
+		thai: 'สิงห์โตเล่นหาง',
+		english: 'A Lion Playing with Its Tail',
+		pronounce: 'Sing-toe-len-hang'
+	},
+	{ thai: 'นางกล่อมตัว', english: 'A Lady Swaying Her Body', pronounce: 'Nang-klom-tuo' },
+	{ thai: 'บัวชูฝัก', english: 'A Blooming Lotus', pronounce: 'Buo-choo-fag' },
+	{ thai: 'ขอนทิ้งอก', english: 'Kon Ting Oak', pronounce: 'Kon-ting-oak' },
+	{
+		thai: 'พระลักษณ์แผลงฤทธิ์',
+		english: "Lakshmana's Rampant",
+		pronounce: 'Pra-lak-plang-rit'
+	},
+	{
+		thai: 'เสือทำลายห้าง',
+		english: 'A Tiger Destroying a Hunter Lookout Tower',
+		pronounce: 'Suae-tam-lai-hang'
+	},
+	{
+		thai: 'ช้างทำลายโรง',
+		english: 'An Elephant Destroying a Shed',
+		pronounce: 'Chang-tam-lai-rong'
+	},
+	{
+		thai: 'ฉุยฉายเข้าวัง',
+		english: 'Gliding into a Palace',
+		pronounce: 'Chui-chai-khao-wang'
+	},
+	{ thai: 'หนังหน้าไฟ', english: 'A Shadow Play Before the Funeral', pronounce: 'Nang-na-fai' },
+	{
+		thai: 'เครือวัลย์พันไม้',
+		english: 'An Ivy Creeping Around a Tree',
+		pronounce: 'Krua-wan-pan-mai'
+	},
+	{ thai: 'เยื้องพายกฐิน', english: 'Yuang Pai Kathin', pronounce: 'Yuang-pai-ka-tin' },
+	{
+		thai: 'มังกรเล่นน้ำ (มังกรเที่ยวหาวาริน)',
+		english: 'A Dragon Playing in the Water',
+		pronounce: 'Mang-korn-len-nam'
+	},
+	{
+		thai: 'ลมพัดยอดตอง',
+		english: 'A Wind Blowing the Top of a Banana Leaf',
+		pronounce: 'Lom-pad-yod-thong'
+	},
+	{
+		thai: 'จีนสาวไส้',
+		english: 'A Chinese Disembowel Themselves',
+		pronounce: 'Jeen-sao-sai'
+	},
+	{ thai: 'วิสัยแทงตรี', english: 'Wi Sai Tang Tri', pronounce: 'Wi-sai-tang-tri' },
+	{
+		thai: 'ชนีร่ายไม้',
+		english: 'A Gibbon Swinging Between the Trees',
+		pronounce: 'Cha-nee-rai-mai'
+	},
+	{
+		thai: 'ขี่ม้าเลียบค่าย',
+		english: 'Riding a Horse Along a Camp',
+		pronounce: 'Kee-ma-leab-kai'
+	},
+	{
+		thai: 'นกยูงฟ้อนหาง (มยุเรศฟ้อนหาง)',
+		english: 'A Peacock Dancing',
+		pronounce: 'Nok-yoong-fon-hang'
+	},
+	{ thai: 'ขัดจังนาง', english: 'Kad Jang Nang', pronounce: 'Kad-jang-nang' },
+	{
+		thai: 'เมขลาโยนแก้ว',
+		english: 'Mekkala Tossing a Glass Orb',
+		pronounce: 'Mek-ka-la-yon-kaew'
+	},
+	{
+		thai: 'กระต่ายต้องแร้ว',
+		english: 'A Rabbit Being Caught in a Spring Trap',
+		pronounce: 'Kra-tai-thong-raew'
+	},
+	{
+		thai: 'โจงกระเบนตีเหล็ก',
+		english: 'Wearing a Loincloth and Forging Steel',
+		pronounce: 'Jong-kra-ben-tee-lek'
+	},
+	{ thai: 'บังพระสุริยา', english: 'Blocking the Sun', pronounce: 'Bang-pra-su-ri-ya' },
+	{
+		thai: 'ชักกระบี่สี่ท่า',
+		english: 'Four Ways of Drawing a Sword',
+		pronounce: 'Chak-kra-bee-see-ta'
+	},
+	{ thai: 'ประไลยวาต', english: 'A Catastrophic Wind', pronounce: 'Pra-lai-wat' },
+	{ thai: 'กินนรรำ', english: 'A Kinnorn Dancing ', pronounce: 'Kin-norn-ram' }
+];
